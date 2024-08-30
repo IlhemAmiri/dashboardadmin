c0a40d932a4a8edb044a3f7004bd3958ed46a52e
Ilhem <ilhem.amiri22@gmail.com>
2024-08-30 12:42:20 +0200

    g

--- src/app/fich/[reservationId]/page.jsx
+++ src/app/fich/[reservationId]/page.jsx
@@ -12,6 +12,8 @@ const PaymentRecordPage = ({ params }) => {
   const [reservation, setReservation] = useState(null);
   const [socialMedia, setSocialMedia] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [discount, setDiscount] = useState(0); // New state for discount
+  const [applyVAT, setApplyVAT] = useState(false); // New state for VAT
   const router = useRouter();
   const printRef = useRef();
   const { reservationId } = params;
@@ -52,6 +54,19 @@ const PaymentRecordPage = ({ params }) => {
     fetchSocialMedia();
   }, [reservationId, router]);
 
+  const calculateNbJours = (start, end) => {
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+    const diffTime = Math.abs(endDate - startDate);
+    return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  };
+
+  const calculateDiscountedPrice = () => {
+    const basePrice = reservation.tarifTotale;
+    const discountedPrice = basePrice * ((100 - discount) / 100);
+    return applyVAT ? discountedPrice * 1.19 : discountedPrice;
+  };
+
   const handleDownload = () => {
     const input = printRef.current;
     html2canvas(input)
@@ -75,62 +90,108 @@ const PaymentRecordPage = ({ params }) => {
     return <div>Data not found</div>;
   }
 
-  const calculateNbJours = (start, end) => {
-    const startDate = new Date(start);
-    const endDate = new Date(end);
-    const diffTime = Math.abs(endDate - startDate);
-    return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-  };
-
   return (
     <div className="flex flex-col md:flex-row min-h-screen">
       <SideNavbar />
       <div className="flex-1 ml-0 md:ml-60 py-8 px-4 transition-all duration-300">
-      <div className=" mx-auto p-8" ref={printRef}>
-          <h1 className="text-3xl font-bold mb-8 text-center">Payment Record</h1>
+        <div className="mx-auto p-8" ref={printRef}>
+          <h1 className="text-3xl font-bold mb-8 text-center">
+            Payment Record
+          </h1>
 
           <div className="mb-8 flex justify-between">
             <div>
-              <h2 className="text-2xl font-semibold mb-4">Company Information</h2>
-              <p><strong>Name:</strong> Rentay</p>
-              <p><strong>Email:</strong> {socialMedia.email}</p>
-              <p><strong>Phone:</strong> {socialMedia.numTel}</p>
-              <p><strong>Location:</strong> {socialMedia.localisation}</p>
+              <h2 className="text-2xl font-semibold mb-4">
+                Company Information
+              </h2>
+              <p>
+                <strong>Name:</strong> Rentay
+              </p>
+              <p>
+                <strong>Email:</strong> {socialMedia.email}
+              </p>
+              <p>
+                <strong>Phone:</strong> {socialMedia.numTel}
+              </p>
+              <p>
+                <strong>Location:</strong> {socialMedia.localisation}
+              </p>
             </div>
             <div>
-              <h2 className="text-2xl font-semibold mb-4">Client Information</h2>
-              <p><strong>Name:</strong> {reservation.idClient.nom} {reservation.idClient.prenom}</p>
-              <p><strong>Email:</strong> {reservation.idClient.email}</p>
-              <p><strong>Address:</strong> {reservation.idClient.adresse}</p>
-              <p><strong>Phone:</strong> {reservation.idClient.numTel}</p>
+              <h2 className="text-2xl font-semibold mb-4">
+                Client Information
+              </h2>
+              <p>
+                <strong>Name:</strong> {reservation.idClient.nom}{" "}
+                {reservation.idClient.prenom}
+              </p>
+              <p>
+                <strong>Email:</strong> {reservation.idClient.email}
+              </p>
+              <p>
+                <strong>Address:</strong> {reservation.idClient.adresse}
+              </p>
+              <p>
+                <strong>Phone:</strong> {reservation.idClient.numTel}
+              </p>
             </div>
           </div>
 
           <div>
-            <h2 className="text-2xl font-semibold mb-4 text-center">Reservation Information</h2>
+            <h2 className="text-2xl font-semibold mb-4 text-center">
+              Reservation Information
+            </h2>
             <table className="min-w-full bg-white mb-8 border border-gray-200">
               <thead>
                 <tr className="border-b bg-gray-100">
-                  <th className="py-2 px-4 font-semibold text-lg text-left border-r border-gray-200">Car Details</th>
-                  <th className="py-2 px-4 font-semibold text-lg text-left border-r border-gray-200">Start Date</th>
-                  <th className="py-2 px-4 font-semibold text-lg text-left border-r border-gray-200">End Date</th>
-                  <th className="py-2 px-4 font-semibold text-lg text-left border-r border-gray-200">Payment Status</th>
-                  <th className="py-2 px-4 font-semibold text-lg text-left border-r border-gray-200">Pickup Location</th>
-                  <th className="py-2 px-4 font-semibold text-lg text-left border-r border-gray-200">Destination</th>
-                  <th className="py-2 px-4 font-semibold text-lg text-left border-r border-gray-200">Price per Day</th>
-                  <th className="py-2 px-4 font-semibold text-lg text-left border-r border-gray-200">Number of Days</th>
-                  <th className="py-2 px-4 font-semibold text-lg text-left">Total Price</th>
+                  <th className="py-2 px-4 font-semibold text-lg text-left border-r border-gray-200">
+                    Car Details
+                  </th>
+                  <th className="py-2 px-4 font-semibold text-lg text-left border-r border-gray-200">
+                    Start Date
+                  </th>
+                  <th className="py-2 px-4 font-semibold text-lg text-left border-r border-gray-200">
+                    End Date
+                  </th>
+                  <th className="py-2 px-4 font-semibold text-lg text-left border-r border-gray-200">
+                    Payment Status
+                  </th>
+                  <th className="py-2 px-4 font-semibold text-lg text-left border-r border-gray-200">
+                    Pickup Location
+                  </th>
+                  <th className="py-2 px-4 font-semibold text-lg text-left border-r border-gray-200">
+                    Destination
+                  </th>
+                  <th className="py-2 px-4 font-semibold text-lg text-left border-r border-gray-200">
+                    Price per Day
+                  </th>
+                  <th className="py-2 px-4 font-semibold text-lg text-left border-r border-gray-200">
+                    Number of Days
+                  </th>
+                  <th className="py-2 px-4 font-semibold text-lg text-left">
+                    Total Price
+                  </th>
                 </tr>
               </thead>
               <tbody>
                 <tr className="border-b hover:bg-gray-50">
                   <td className="py-2 px-4 border-r border-gray-200">
-                  <p><strong>ID Vehicle:</strong> {reservation.idVoiture._id}</p>
-                    <p><strong>Vehicle Type:</strong> {reservation.idVoiture.vehicleType}</p>
-                    <p><strong>Make and Model:</strong> {reservation.idVoiture.marque} {' '}{reservation.idVoiture.modele}</p>
-                    <p><strong>Year:</strong> {reservation.idVoiture.anneeFabrication}</p>
-                    
-                    
+                    <p>
+                      <strong>ID Vehicle:</strong> {reservation.idVoiture._id}
+                    </p>
+                    <p>
+                      <strong>Vehicle Type:</strong>{" "}
+                      {reservation.idVoiture.vehicleType}
+                    </p>
+                    <p>
+                      <strong>Make and Model:</strong>{" "}
+                      {reservation.idVoiture.marque}{" "}
+                      {reservation.idVoiture.modele}
+                    </p>
+                    <p>
+                      <strong>Year:</strong>{" "}
+                      {reservation.idVoiture.anneeFabrication}
+                    </p>
                   </td>
                   <td className="py-2 px-4 border-r border-gray-200">
                     {new Date(reservation.dateDebut).toLocaleDateString()}
@@ -138,19 +199,95 @@ const PaymentRecordPage = ({ params }) => {
                   <td className="py-2 px-4 border-r border-gray-200">
                     {new Date(reservation.dateFin).toLocaleDateString()}
                   </td>
-                  <td className="py-2 px-4 border-r border-gray-200">{reservation.statusPaiement}</td>
-                  <td className="py-2 px-4 border-r border-gray-200">{reservation.lieuRamassage}</td>
-                  <td className="py-2 px-4 border-r border-gray-200">{reservation.destination}</td>
-                  <td className="py-2 px-4 border-r border-gray-200">{reservation.idVoiture.prixParJ} $</td>
                   <td className="py-2 px-4 border-r border-gray-200">
-                    {calculateNbJours(reservation.dateDebut, reservation.dateFin)} days
+                    {reservation.statusPaiement}
                   </td>
-                  <td className="py-4 px-4 font-semibold text-xl">{reservation.tarifTotale} $</td>
+                  <td className="py-2 px-4 border-r border-gray-200">
+                    {reservation.lieuRamassage}
+                  </td>
+                  <td className="py-2 px-4 border-r border-gray-200">
+                    {reservation.destination}
+                  </td>
+                  <td className="py-2 px-4 border-r border-gray-200">
+                    {reservation.idVoiture.prixParJ} $
+                  </td>
+                  <td className="py-2 px-4 border-r border-gray-200">
+                    {calculateNbJours(
+                      reservation.dateDebut,
+                      reservation.dateFin
+                    )}{" "}
+                    days
+                  </td>
+                  <td className="py-4 px-4 font-semibold text-xl">
+                    {reservation.tarifTotale} $
+                  </td>
                 </tr>
               </tbody>
             </table>
           </div>
+
+          <div className="mb-4 p-4 border border-gray-300 rounded-lg bg-gray-50">
+            <h2 className="text-2xl font-semibold mb-4">Payment Adjustments</h2>
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                {/* Display discount as text for PDF */}
+                <p className="mt-2 text-lg font-medium">
+                  Applied Discount: {discount}%
+                </p>
+              </div>
+              <div>
+                {/* Display VAT choice as text for PDF */}
+                <p className="mt-2 text-lg font-medium">
+                  VAT Applied: {applyVAT ? "Yes" : "No"}
+                </p>
+              </div>
+            </div>
+            <div className="text-right text-xl font-semibold mt-8">
+              <p>
+                Price after Discount and VAT:{" "}
+                <span className="text-blue-600">
+                  {calculateDiscountedPrice().toFixed(2)} $
+                </span>
+              </p>
+            </div>
+          </div>
         </div>
+
+        <div className="m-8 p-8 border border-gray-300 rounded-lg bg-gray-50">
+          <div className="mb-4">
+            <label className="block text-lg font-medium mb-2">
+              Apply VAT (19%)
+            </label>
+            <select
+              value={applyVAT}
+              onChange={(e) => setApplyVAT(e.target.value === "true")}
+              className="border border-gray-300 rounded px-4 py-2 w-full"
+            >
+              <option value="false">No</option>
+              <option value="true">Yes</option>
+            </select>
+          </div>
+
+          <div>
+            <label className="block text-lg font-medium mb-2">
+              Discount (%)
+            </label>
+            <input
+              type="number"
+              value={discount}
+              onChange={(e) => {
+                const value = Math.max(
+                  0,
+                  Math.min(100, Number(e.target.value))
+                );
+                setDiscount(value);
+              }}
+              className="border border-gray-300 rounded px-4 py-2 w-full"
+              placeholder="Enter discount percentage"
+            />
+          </div>
+        </div>
+
         <div className="text-center">
           <button
             onClick={handleDownload}
